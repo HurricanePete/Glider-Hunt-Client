3667b49746c70cf9ac921db3f38abaf49a3b7a70
AaronFried <AaronFried@users.noreply.github.com>
2018-03-22 21:21:35 -0400

    Improve hero copy

--- src/components/hero.js
+++ src/components/hero.js
@@ -5,10 +5,10 @@ import './styles/hero.css';
 export default function Hero(props) {
 	return(
 		<section className="hero row pv3">
-			<h1 className="logo-name ma0">Get Your Wings . . .</h1>
-			<h2>This is my subtitle</h2>
+			<h1 className="logo-name ma0">Get Your Wings</h1>
+			<h2>Find the top gliders and training in your area.</h2>
 			<div className="hero-wrapper col-3 clear-float center tc">
-				<h3>Looking for top training and dealers?</h3>
+				<h3>Don't know where to start?</h3>
 				<button className="submit match pv2 ph2" type="submit">Match Me</button> 
 			</div>
 		</section>
